61d02fc78f7464116111b8a62e4b2f45c7ab73b0
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-08-15 15:12:30 -0300

    feat: styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
