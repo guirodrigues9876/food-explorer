a7b885015e919e336964ebf5aea04cac4ef15008
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-08-23 13:55:59 -0300

    style: card transform

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -11,11 +11,12 @@ export const Container = styled.section`
         color: ${({ theme }) => theme.COLORS.GRAY_100};
         margin-bottom: 2.4rem;
     }
-
+    
     @media (min-width: 1024px){
         
         > h2 {
             font-size: 3.2rem;
+            margin-bottom: 0rem;
         }
     }
 `;
